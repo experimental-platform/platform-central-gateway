629ac5758a90442368e9d31c74196158e35dacd2
Andreas Litt <andreas.litt@gmail.com>
2015-06-16 11:09:07 +0200

    add some tests

--- ci-build.sh
+++ ci-build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # THIS ONLY WORK IN OUR CI!
 
-docker run --rm -v /data/jenkins/jobs/${JOB_NAME}/workspace:/usr/src/central-gateway -w /usr/src/central-gateway golang:1.4 /bin/bash -c 'go get -d && go build -v'
+docker run --rm -v /data/jenkins/jobs/${JOB_NAME}/workspace:/usr/src/central-gateway -w /usr/src/central-gateway golang:1.4 /bin/bash -c 'go get -d && go test && go build -v'
---
+++ server_test.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"net/http/httputil"
+	"net/url"
+	"os"
+	"testing"
+)
+
+func TestRedirectToAdminOnSlash(t *testing.T) {
+	req, err := http.NewRequest("GET", "http://example.com", nil)
+	assert.Nil(t, err)
+
+	w := httptest.NewRecorder()
+	defaultHandler(w, req)
+	assert.Equal(t, w.Code, http.StatusMovedPermanently)
+}
+
+func TestNoRedirectToAdminOnSlash(t *testing.T) {
+	req, err := http.NewRequest("GET", "http://example.com/", nil)
+	assert.Nil(t, err)
+
+	w := httptest.NewRecorder()
+	defaultHandler(w, req)
+	assert.Equal(t, http.StatusMovedPermanently, w.Code)
+	assert.Equal(t, "/admin/", w.Header().Get("Location"))
+}
+
+func TestAppAccess(t *testing.T) {
+	req, err := http.NewRequest("GET", "http://example.com/foobar", nil)
+	assert.Nil(t, err)
+
+	w := httptest.NewRecorder()
+	defaultHandler(w, req)
+	assert.NotEqual(t, http.StatusMovedPermanently, w.Code)
+}
+
+func TestAdminAccess(t *testing.T) {
+	req, err := http.NewRequest("GET", "http://example.com/admin/", nil)
+	assert.Nil(t, err)
+
+	w := httptest.NewRecorder()
+	defaultHandler(w, req)
+	assert.NotEqual(t, http.StatusMovedPermanently, w.Code)
+}
+
+func TestAdminAccessRedirectSlash(t *testing.T) {
+	req, err := http.NewRequest("GET", "http://example.com/admin", nil)
+	assert.Nil(t, err)
+
+	w := httptest.NewRecorder()
+	defaultHandler(w, req)
+	assert.Equal(t, http.StatusMovedPermanently, w.Code)
+	assert.Equal(t, "/admin/", w.Header().Get("Location"))
+}
+
+func TestMain(m *testing.M) {
+	target_url, _ := url.Parse("http://example.com/")
+	apps_proxy = httputil.NewSingleHostReverseProxy(target_url)
+	management_proxy = httputil.NewSingleHostReverseProxy(target_url)
+	exit := m.Run()
+	os.Exit(exit)
+}
